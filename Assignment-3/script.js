e64fe2fec237bc22e2db91fa9d2eafaadc438fef
Vishalmane0196 <vishal.mane@emergys.com>
2025-01-17 11:48:33 +0530

    Make Ui Responsive and Add sort Functionality

--- Assignment-3/script.js
+++ Assignment-3/script.js
@@ -3,15 +3,18 @@ document.addEventListener('DOMContentLoaded', () => {
     let todos = JSON.parse(localStorage.getItem('todos')) || [];
     var audio = new Audio('y2mate (mp3cut.net).mp3');
     var deleteaudio = new Audio('deleteaudio.mp3');
-
-    document.getElementsByClassName('sub-btn')[0].style.opacity = '0.4';  // opacity of submit button
+    document.getElementsByClassName('home')[0].classList.add('liactive');
+    document.getElementsByClassName('sub-btn')[0].style.opacity = '0.4';
+    document.getElementsByClassName('sub-btn')[0].disabled = true;   // opacity of submit button
 
     document.getElementById('title').addEventListener('input', function (e) {
         if (e.data == null || document.getElementById('title').value == '') {
             document.getElementsByClassName('sub-btn')[0].style.opacity = '0.4';
+            document.getElementsByClassName('sub-btn')[0].disabled = true; 
         }
         else {
             document.getElementsByClassName('sub-btn')[0].style.opacity = '1';
+            document.getElementsByClassName('sub-btn')[0].disabled = false; 
         }
     })
 
@@ -23,6 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementsByClassName('form1')[0].style.display = 'block';
         document.getElementsByClassName('addtoli')[0].classList.add('liactive');
         document.getElementsByClassName('view-todo')[0].classList.remove('liactive');
+        document.getElementsByClassName('home')[0].classList.remove('liactive');
 
     })
 
@@ -33,14 +37,22 @@ document.addEventListener('DOMContentLoaded', () => {
         if (truetodo.length === 0) {
             ul[0].innerHTML = `<li style="color:white; width:100%; height:96%; display:flex; align-items:center;justify-content:center; font-size:2rem; color:gray;"> No todos</li>`;
             document.querySelector('.clear-btn').style.opacity = '0.2';
+            document.querySelector('.clear-btn').disabled = true;
+            document.getElementsByClassName('search')[0].disabled =true;
+            document.getElementById('sort').disabled =true
+
             return;
         }
         if (todos.length === 0) {
             ul[0].innerHTML = `<li style="color:white; width:100%; height:96%; display:flex; align-items:center;justify-content:center; font-size:2rem; color:gray;"> No todos</li>`;
             document.querySelector('.clear-btn').style.opacity = '0.2';
+            document.querySelector('.clear-btn').disabled = true;
+            document.getElementsByClassName('search')[0].disabled =true;
+            document.getElementById('sort').disabled =true
 
             return;
         } else {
+
             DisplayTodos(todos);
         }
     }
@@ -74,7 +86,11 @@ document.addEventListener('DOMContentLoaded', () => {
             todobtn[0].classList.add('active');
             DisplayTodos(todos);
             document.querySelector('.clear-btn').style.opacity = '1';
+            document.querySelector('.clear-btn').disabled = false;
+            document.getElementsByClassName('search')[0].disabled =false;
+            document.getElementById('sort').disabled =false;
             document.getElementsByClassName('sub-btn')[0].style.opacity = '0.4';
+            document.getElementsByClassName('sub-btn')[0].disabled = true; 
             this.reset();
         }
     });
@@ -85,7 +101,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (truetodo.length === 0) {
             ul.innerHTML = `<li style="color:white; width:100%; height:96%; display:flex; align-items:center;justify-content:center; font-size:2rem; color:gray;"> No todos</li>`;
-            document.querySelector('.clear-btn').style.opacity = '0.2';
+            // document.querySelector('.clear-btn').style.opacity = '0.2';
+            // document.querySelector('.clear-btn').disabled = true;
+            // document.getElementsByClassName('search')[0].disabled =true;
+            // document.getElementById('sort').disabled =true;
             return;
         }
         if (todos.length === 0) {
@@ -126,6 +145,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (todos.length === 0) {
             ul[0].innerHTML = `<li style="color:gray; width:100%; height:96%; display:flex; align-items:center;justify-content:center; font-size:2rem"> No todos</li>`;
+            document.querySelector('.clear-btn').style.opacity = '0.2';
+            document.querySelector('.clear-btn').disabled = true;
+            document.getElementsByClassName('search')[0].disabled =true;
+            document.getElementById('sort').disabled =true
             counttotal();
             return;
         } else {
@@ -173,7 +196,9 @@ document.addEventListener('DOMContentLoaded', () => {
                     DisplayTodoscompleted(completedTodos);
                 } else {
                     playaudio();
+                    counttotal();
                     DisplayTodos(todos);
+
                 }
             });
         });
@@ -262,7 +287,11 @@ document.addEventListener('DOMContentLoaded', () => {
         if (completedTodos.length === 0) {
             const ul = document.getElementsByClassName('ul-list')[0];
             ul.innerHTML = `<li style="color:gray; width:100%; height:96%; display:flex; align-items:center;justify-content:center; font-size:2rem;"> All task completed</li>`;
+            
         } else {
+
+            document.getElementsByClassName('search')[0].disabled =false;
+            document.getElementById('sort').disabled =false
             const ul = document.getElementsByClassName('ul-list')[0];
             ul.innerHTML = '';
 
@@ -297,16 +326,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const clear_btn = document.getElementsByClassName('clear-btn');
     clear_btn[0].addEventListener('click', function () {
-        if (todos.length === 0) {
-            alert('No tasks to clear..');
-            return;
-        } else {
-            if (confirm('Are you sure you want to delete all todos?')) {
-                counttotal();
-                localStorage.removeItem('todos');
-                window.location.reload();
-            }
-        }
+                document.getElementsByClassName('popup-overlay')[0].style.display = 'flex';
+                document.getElementsByClassName('confirm-btn-delete')[0].addEventListener('click',function(){
+                    counttotal();
+                    localStorage.removeItem('todos');
+                    window.location.reload();
+                   
+                })
+                document.getElementsByClassName('cancel-btn-delete')[0].addEventListener('click',function(){
+                    
+                    document.getElementsByClassName('popup-overlay')[0].style.display = 'none';
+                    counttotal();
+                   
+                })   
     });
     function playaudio() {
         audio.play();
@@ -324,17 +356,18 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementsByClassName('list')[0].style.display = 'block';
         document.getElementsByClassName('addtoli')[0].classList.remove('liactive');
         document.getElementsByClassName('view-todo')[0].classList.add('liactive');
+        document.getElementsByClassName('home')[0].classList.remove('liactive');
 
     })
-
-
+    let newarray = [];
+   
     //Search Bar
     document.getElementsByClassName('search')[0].addEventListener('input', function (e) {
 
-        let searchValue = document.getElementsByClassName('search')[0].value;
+       let  searchValue = document.getElementsByClassName('search')[0].value;
         const todobtn = document.getElementsByClassName('todo-btn');
         // let newcomarray = todos.filter(todo=>todo.done == true);
-        let newarray = [];
+         newarray = [];
         if (searchValue.includes(" ") || e.data == null) {
             if (todobtn[0].classList.contains('active')) {
                 console.log('i am here')
@@ -374,15 +407,31 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    //---------------Home
+
+    document.getElementsByClassName('home')[0].addEventListener('click',function(){
+
+        document.getElementsByClassName('main1')[0].style.display = 'block';
+        document.getElementsByClassName('inner')[0].style.display = 'none';
+        document.getElementsByClassName('list')[0].style.display = 'none';
+        document.getElementsByClassName('home')[0].classList.add('liactive');
+        document.getElementsByClassName('view-todo')[0].classList.remove('liactive');
+        document.getElementsByClassName('addtoli')[0].classList.remove('liactive');
+    
+    })
+
 
     //-------------Sorting
 
 
     document.getElementById('sort').addEventListener('click', function () {
+        if(newarray.length === 0){
+            newarray = todos;
+        }
         let sortedArray = [];
         let sortType = document.getElementById('sort').value;
         if (sortType == 'AZ') {
-            sortedArray = todos.slice().sort((a, b) => a.title.localeCompare(b.title))
+            sortedArray = newarray.slice().sort((a, b) => a.title.localeCompare(b.title))
             if (todobtn[0].classList.contains('active')) {
                 DisplayTodos(sortedArray);
                 return;
@@ -394,7 +443,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         }
         else if (sortType == 'ZA') {
-            sortedArray = todos.slice().sort((a, b) => b.title.localeCompare(a.title))
+            sortedArray = newarray.slice().sort((a, b) => b.title.localeCompare(a.title))
             if (todobtn[0].classList.contains('active')) {
                 DisplayTodos(sortedArray);
                 return;
@@ -405,7 +454,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
         else if (sortType == 'latest') {
-            sortedArray = todos.slice().sort((a, b) => new Date(b.due_date) - new Date(a.due_date));
+            sortedArray = newarray.slice().sort((a, b) => new Date(b.due_date) - new Date(a.due_date));
 
             if (todobtn[0].classList.contains('active')) {
                 DisplayTodos(sortedArray);
@@ -417,7 +466,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
         else if (sortType == 'date') {
-            sortedArray = todos.slice().sort((a, b) => new Date(a.due_date) - new Date(b.due_date));
+            sortedArray = newarray.slice().sort((a, b) => new Date(a.due_date) - new Date(b.due_date));
 
             if (todobtn[0].classList.contains('active')) {
                 DisplayTodos(sortedArray);
@@ -446,7 +495,18 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementsByClassName('popup')[0].style.display = 'none';
         document.getElementById('overlay').style.display = 'none';
     })
+    let y =true;
 
+    document.getElementsByClassName('profi')[0].addEventListener('click',function(){
+       if(y){
+         document.getElementsByClassName('side')[0].style.display = 'none'
+         y = false;
+       }
+       else{
+         document.getElementsByClassName('side')[0].style.display = 'flex'
+         y = true;
+       }
+    })
 
 
     notask();
